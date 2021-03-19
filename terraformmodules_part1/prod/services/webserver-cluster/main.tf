59d7198b771de408bc406a525cbe6eb9a6be8678
CVERAMENDI <christiand.veramendi@gmail.com>
2021-03-19 11:24:58 -0500

    lab08 part1

---
+++ terraformmodules_part1/prod/services/webserver-cluster/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+module "webserver_cluster" {
+  source                 = "../../../modules/services/webserver-cluster"
+  cluster_name           = "webservers-prod"
+  db_remote_state_bucket = "terraform-up-and-running-state-cvlayout"
+  db_remote_state_key    = "prod/data-stores/mysql/terraform.tfstate"
+  instance_type          = "m4.large"
+  min_size               = 2
+  max_size               = 10
+}
+
