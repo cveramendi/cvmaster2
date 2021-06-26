46d08d6a20e501897056ca51b34a119168e77426
CVERAMENDI <christiand.veramendi@gmail.com>
2021-06-25 19:16:43 -0500

    LAB09 - LOOPS

---
+++ terraformmodules_loops/live/global/iam/main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-west-1"
+}
+
+/*
+resource "aws_iam_user" "examplecv" {
+  name = "cvdon"
+}
+
+resource "aws_iam_user" "example" {
+  count = length(var.user_names)
+  name  = var.user_names[count.index]
+}
+*/
+resource "aws_iam_user" "example" {
+  for_each = toset(var.user_names)
+  name     = each.value
+}
